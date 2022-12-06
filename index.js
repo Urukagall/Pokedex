b1dd91a9a1faac079d143223e76108e052f396a2
Urukagall <dany.jorge@free.fr>
2022-12-06 11:45:40 +0100

    tout fait

--- index.js
+++ index.js
@@ -17,39 +17,117 @@ app.listen(port, function () {
   console.log(`App listening on port ${port}!`);
 });
 app.get("/pokemon/list", function (req, res) {
-    //on se connecte à la DB MongoDB
-    const dbConnect = dbo.getDb();
-    //premier test permettant de récupérer mes pokemons !
-    dbConnect
-      .collection("Pokemon")
-      .find({}) // permet de filtrer les résultats
-      /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
-      .toArray(function (err, result) {
+  //on se connecte à la DB MongoDB
+  const dbConnect = dbo.getDb();
+  //premier test permettant de récupérer mes pokemons !
+  dbConnect
+    .collection("Pokemon")
+    .find({}) // permet de filtrer les résultats
+    /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching pokemons!");
+      } else {
+        res.json(result);
+      }
+    });
+});
+
+app.post('/pokemon/insert', jsonParser, (req, res) => {
+  const body = req.body;
+  console.log('Got body:', req.body);
+
+  const dbConnect = dbo.getDb();
+
+  const poke = dbConnect.collection("Pokemon")
+  const type = dbConnect.collection("Type")
+  
+  type.findOne({type: body.type1}).then(function (result,err) {
+    if (err) {
+      res.status(400).send("Error fetching pokemons!");
+    } else {
+      const type1 = result
+      poke.insertOne({name: body.name,type: [result]})
+      res.json(result);
+      if (body.type2 !== null) {
+        type.findOne({type: body.type2}).then(function (result1,err1) {
+          if (err1) {
+            res.status(400).send("Error fetching pokemons!");
+          } else {
+            poke.replaceOne({name: body.name}, {name: body.name,type : [type1, result1]})
+            res.json(result1);
+          }
+        });
+      }
+    }
+  });
+  
+}); 
+
+
+app.get('/pokemon/read', jsonParser, (req, res) => {
+  const body = req.body;
+  console.log('Got body:', body);
+
+  const dbConnect = dbo.getDb();
+
+  const poke = dbConnect.collection("Pokemon")
+  const type = dbConnect.collection("Type")
+  poke.findOne({name: body.name}).then(function (result,err) {
+    if (err) {
+      res.status(400).send("Error fetching pokemons!");
+    } else {
+      res.json(result);
+    }
+  });
+}); 
+
+app.post('/pokemon/update', jsonParser, (req, res) => {
+  const body = req.body;
+  console.log('Got body:', req.body);
+
+  const dbConnect = dbo.getDb();
+
+  const poke = dbConnect.collection("Pokemon")
+  const type = dbConnect.collection("Type")
+  const options = { upsert: true };
+  
+  type.findOne({type: body.type1}).then(function (result,err) {
+    if (err) {
+      res.status(400).send("Error fetching pokemons!");
+    } else {
+      const type1 = result
+      poke.updateOne({name: body.name}, {$set: {type: [result]}}, options)
+      type.findOne({type: body.type2}).then(function (result1,err1) {
         if (err) {
           res.status(400).send("Error fetching pokemons!");
         } else {
-          res.json(result);
+          if (body.type2 !== null) {
+            poke.updateOne({name: body.name}, {$set: {type: [type1,result1]}}, options)
+          }
+          res.json(result1);
         }
       });
-      /*
-      Bref lisez la doc, 
-      il y a plein de manières de faire ce qu'on veut :) 
-      */
-      
+    }
   });
+}); 
 
-  app.post('/pokemon/insert', jsonParser, (req, res) => {
-    const body = req.body;
-    console.log('Got body:', req.body);
 
-    const dbConnect = dbo.getDb();
+app.delete('/pokemon/delete', jsonParser, (req, res) => {
+  const body = req.body;
+  console.log('Got body:', req.body);
 
-    const poke = dbConnect.collection("Pokemon")
-    const type = dbConnect.collection("Type")
-    
-    
-    poke.insertOne({name: req.body.name , type: "fire"})
-    console.log(poke.findOne({name: "pikachu"}))
+  const dbConnect = dbo.getDb();
 
-    res.json(req.body.name);
+  const poke = dbConnect.collection("Pokemon")
+  const type = dbConnect.collection("Type")
+  
+  
+  poke.deleteOne({name: body.name}).then(function (result,err) {
+    if (err) {
+      res.status(400).send("Error fetching pokemons!");
+    } else {
+      res.json(result);
+    }
+  });
 });
