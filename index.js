98992e17db4bda46e64de2efcd677f9d2134f2aa
Urukagall <dany.jorge@free.fr>
2022-12-13 11:32:02 +0100

    d

--- index.js
+++ index.js
@@ -164,6 +164,26 @@ app.delete('/pokemon/delete', jsonParser, (req, res) => {
   });
 });
 
+app.get("/pokemon/listtype?type=", function (req, res) {
+  //on se connecte à la DB MongoDB
+  const dbConnect = dbo.getDb();
+
+  const poke = dbConnect.collection("Pokemon")
+  //premier test permettant de récupérer mes pokemons !
+  dbConnect
+    .collection("Pokemon")
+
+    .find({"type.type":req.query.type}) // permet de filtrer les résultats
+    /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching pokemons!");
+      } else {
+        res.json(result, type);
+      }
+    });
+});
+
 app.post('/pokedex/insert', jsonParser, (req, res) => {
   const body = req.body;
   console.log('Got body:', req.body);
